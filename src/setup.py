7112d6d84dee644ab4e2e56dd83a677344f2d750
sumitpatra6 <sumitpatra6@gmail.com>
2022-08-18 00:49:45 +0530

    First commit

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+
+setup(
+    name = "lambda tutorial",
+    version="1.0",
+    description="Python lambda project",
+    author="Sumit Patra",
+    author_email="sumitpatra6@gmail.com"
+)
