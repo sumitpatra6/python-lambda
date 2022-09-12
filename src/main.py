d5a357f43bc4de609d8663e1ce6a0b6e5aaa9905
sumitpatra6 <sumitpatra6@gmail.com>
2022-09-13 01:03:39 +0530

    Dynamo db integration

--- src/main.py
+++ src/main.py
@@ -1,18 +1,40 @@
+import os
 import json
 import logging
-import json
+import boto3
 
 logger = logging.getLogger()
+if os.getenv('ENV'):
+    dynamodb = boto3.resource('dynamodb')
+
+def save_item(item):
+    print("Saving items")
+    table = dynamodb.Table('TestTable')
+    table.put_item(Item=item)
+
+def get_all_items():
+    print("Querying dynamo db")
+    table = dynamodb.Table('TestTable')
+    return table.scan()
 
 def main(event, context):
     print(event)
-    operation = event.get("httpMethod", "")
-    payload = event.get("body", {})
-    logger.info(event)
-    return {
+    method = event.get("httpMethod", "")
+    body = event.get("body", {})
+    if method == "POST":
+        save_item(body)
+        return {
         'statusCode': 200,
         'body': json.dumps({
-            'event' : event
+            'message' : "Success"
         })
         
+    }
+
+    if method == "GET":
+        return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'data' : get_all_items()
+        })
     }
