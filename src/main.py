1bc6cad47ee55a369c8ec3a67ac1197f9c780f47
sumitpatra6 <sumitpatra6@gmail.com>
2022-09-27 00:31:22 +0530

    Updated dynamo db code

--- src/main.py
+++ src/main.py
@@ -4,37 +4,44 @@ import logging
 import boto3
 
 logger = logging.getLogger()
-if os.getenv('ENV'):
-    dynamodb = boto3.resource('dynamodb')
+
+dynamodb = boto3.resource('dynamodb')
 
 def save_item(item):
     print("Saving items")
+    print(item)
     table = dynamodb.Table('TestTable')
-    table.put_item(Item=item)
+    table.put_item(Item=json.loads(item))
 
 def get_all_items():
     print("Querying dynamo db")
     table = dynamodb.Table('TestTable')
-    return table.scan()
+    return table.scan().get('Items', [])
 
 def main(event, context):
-    print(event)
-    method = event.get("httpMethod", "")
-    body = event.get("body", {})
-    if method == "POST":
-        save_item(body)
-        return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'message' : "Success"
-        })
-        
-    }
+    try:
+        print(event)
+        method = event.get("httpMethod", "")
+        body = event.get("body", {})
+        if method == "POST":
+            save_item(body)
+            return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'message' : "Success"
+            })
+            
+        }
 
-    if method == "GET":
+        if method == "GET":
+            return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'data' : get_all_items()
+            })
+        }
+    except Exception as exp:
         return {
-        'statusCode': 200,
-        'body': json.dumps({
-            'data' : get_all_items()
-        })
-    }
+            'statusCode': 500,
+            'body': str(exp)
+        }
